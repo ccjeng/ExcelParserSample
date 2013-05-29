223457f16c1f3f9d8b9af6287a09c6c206c20a7c
Andy Cheng <ccjeng@gmail.com>
2013-05-29 23:48:33 +0800

    initial

---
+++ Row.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ExcelParser
+{
+    public class Row
+    {
+         [JsonProperty]
+        public string  Header1 { get; set; }
+        [JsonProperty]
+        public string  Header2 { get; set; }
+        [JsonProperty]
+        public string  Header3 { get; set; }
+        [JsonProperty]
+        public string  Header4 { get; set; }
+        [JsonProperty]
+        public string  Header5 { get; set; }
+        [JsonProperty]
+        public string  Header6 { get; set; }
+        [JsonProperty]
+        public string  Header7 { get; set; }
+        [JsonProperty]
+        public string  Header8 { get; set; }
+        [JsonProperty]
+        public string  Header9 { get; set; }
+        [JsonProperty]
+        public string  Header10 { get; set; }
+        [JsonProperty]
+        public string  Header11 { get; set; }
+        [JsonProperty]
+        public string  Header12 { get; set; }
+        [JsonProperty]
+        public string  Header13 { get; set; }
+        [JsonProperty]
+        public string  Header14 { get; set; }
+        [JsonProperty]
+        public string  Header15 { get; set; }
+        [JsonProperty]
+        public string  Header16 { get; set; }
+        [JsonProperty]
+        public string  Header17 { get; set; }
+        [JsonProperty]
+        public string  Header18 { get; set; }
+        [JsonProperty]
+        public string  Header19 { get; set; }
+        [JsonProperty]
+        public string  Header20 { get; set; }
+        [JsonProperty]
+        public string  Header21 { get; set; }
+        [JsonProperty]
+        public string  Header22 { get; set; }
+        [JsonProperty]
+        public string  Header23 { get; set; }
+        [JsonProperty]      
+        public string  Header24 { get; set; }
+        [JsonProperty]
+        public string  Header25 { get; set; }
+        [JsonProperty]      
+        public string  Header26 { get; set; }
+        [JsonProperty]
+        public string  Header27 { get; set; }
+        [JsonProperty]
+        public string  Header28 { get; set; }
+        [JsonProperty]
+        public string  Header29 { get; set; }
+        [JsonProperty]
+        public string  Header30 { get; set; }
+        [JsonProperty]
+        public string  Header31 { get; set; }
+        [JsonProperty]
+        public string  Header32 { get; set; }
+        [JsonProperty]
+        public string  Header33 { get; set; }
+        [JsonProperty]
+        public string  Header34 { get; set; }
+        [JsonProperty]
+        public string  Header35 { get; set; }
+        [JsonProperty]
+        public string  Header36 { get; set; }
+        [JsonProperty]
+        public string  Header37 { get; set; }
+        [JsonProperty]
+        public string  Header38 { get; set; }
+        [JsonProperty]
+        public string  Header39 { get; set; }
+        [JsonProperty]
+        public string  Header40 { get; set; }
+        [JsonProperty]
+        public string  Header41 { get; set; }
+        [JsonProperty]
+        public string  Header42 { get; set; }
+        [JsonProperty]
+        public string  Header43 { get; set; }
+        [JsonProperty]
+        public string  Header44 { get; set; }
+        [JsonProperty]
+        public string  Header45 { get; set; }
+        [JsonProperty]
+        public string  Header46 { get; set; }
+        [JsonProperty]
+        public string  Header47 { get; set; }
+        [JsonProperty]
+        public string  Header48 { get; set; }
+        [JsonProperty]
+        public string  Header49 { get; set; }
+        [JsonProperty]
+        public string  Header50 { get; set; }
+        [JsonProperty] 
+        public string  Header51 { get; set; }
+        [JsonProperty] 
+        public string  Header52 { get; set; }
+        [JsonProperty]
+        public string  Header53 { get; set; }
+        [JsonProperty]
+        public string  Header54 { get; set; }
+        [JsonProperty]
+        public string  Header55 { get; set; }
+        [JsonProperty]
+        public string  Header56 { get; set; }
+        [JsonProperty]
+        public string  Header57 { get; set; }
+        [JsonProperty] 
+        public string  Header58 { get; set; }
+        [JsonProperty]
+        public string  Header59 { get; set; }
+        [JsonProperty]
+        public string  Header60 { get; set; }
+        [JsonProperty]
+        public string  Header61 { get; set; }
+        [JsonProperty]
+        public string  Header62 { get; set; }
+        [JsonProperty]
+        public string  Header63 { get; set; }
+        [JsonProperty]
+        public string  Header64 { get; set; }
+        [JsonProperty]
+        public string  Header65 { get; set; }
+        [JsonProperty]
+        public string  Header66 { get; set; }
+        [JsonProperty]
+        public string  Header67 { get; set; }
+        [JsonProperty]
+        public string  Header68 { get; set; }
+        [JsonProperty]
+        public string  Header69 { get; set; }
+        [JsonProperty]
+        public string  Header70 { get; set; }
+        [JsonProperty]
+        public string  Header71 { get; set; }
+        [JsonProperty]
+        public string  Header72 { get; set; }
+        [JsonProperty]
+        public string  Header73 { get; set; }
+        [JsonProperty]
+        public string  Header74 { get; set; }
+        [JsonProperty]
+        public string  Header75 { get; set; }
+        [JsonProperty]
+        public string  Header76 { get; set; }
+        [JsonProperty]
+        public string  Header77 { get; set; }
+        [JsonProperty]
+        public string  Header78 { get; set; }
+        [JsonProperty]
+        public string  Header79 { get; set; }
+        [JsonProperty]
+        public string  Header80 { get; set; }
+        [JsonProperty]
+        public string  Header81 { get; set; }
+        [JsonProperty]
+        public string  Header82 { get; set; }
+        [JsonProperty]
+        public string  Header83 { get; set; }
+        [JsonProperty]
+        public string  Header84 { get; set; }
+        [JsonProperty]
+        public string  Header85 { get; set; }
+        [JsonProperty]
+        public string  Header86 { get; set; }
+        [JsonProperty]
+        public string  Header87 { get; set; }
+        [JsonProperty]
+        public string  Header88 { get; set; }
+        [JsonProperty]
+        public string  Header89 { get; set; }
+        [JsonProperty]
+        public string  Header90 { get; set; }
+        [JsonProperty]
+        public string  Header91 { get; set; }
+        [JsonProperty]
+        public string  Header92 { get; set; }
+        [JsonProperty]
+        public string  Header93 { get; set; }
+        [JsonProperty]
+        public string  Header94 { get; set; }
+        [JsonProperty]
+        public string  Header95 { get; set; }
+        [JsonProperty]
+        public string  Header96 { get; set; }
+        [JsonProperty]
+        public string  Header97 { get; set; }
+        [JsonProperty]
+        public string  Header98 { get; set; }
+        [JsonProperty]
+        public string  Header99 { get; set; }
+        [JsonProperty]
+        public string  Header100 { get; set; }
+
+    }
+}
---
+++ Takeio.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using TakeIo.Spreadsheet;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ExcelParser
+{
+    class Takeio
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime startTime = DateTime.Now;
+
+            var inputFile = new FileInfo("..\\..\\File\\Book1500.xlsx"); // could be .xls or .xlsx too
+            var sheet = Spreadsheet.Read(inputFile);
+
+            sheet.RemoveEmptyRows();
+
+            int start = 10; //start from row 10
+            bool beginFind = false;
+
+            List<Row> rowObj = new List<Row>();
+
+            
+
+            for (var i = 0; i < sheet.Count; i++)
+            { 
+                if (i == start)  //start parsing
+                {
+                    beginFind = true;
+                } else if (sheet[i][0]=="Total") //end parsing
+                {
+                    break;
+                }
+                if (beginFind)
+                {
+                    rowObj.Add(new Row {
+                            Header1  = sheet[i][0],
+                            Header2  = sheet[i][1],
+                            Header3  = sheet[i][2],
+                            Header4  = sheet[i][3],
+                            Header5  = sheet[i][4],
+                            Header6  = sheet[i][5],
+                            Header7  = sheet[i][6],
+                            Header8  = sheet[i][7],
+                            Header9  = sheet[i][8],
+                            Header10 = sheet[i][9],
+                            Header11 = sheet[i][10],
+                            Header12 = sheet[i][11],
+                            Header13 = sheet[i][12],
+                            Header14 = sheet[i][13],
+                            Header15 = sheet[i][14],
+                            Header16 = sheet[i][15],
+                            Header17 = sheet[i][16],
+                            Header18 = sheet[i][17],
+                            Header19 = sheet[i][18],
+                            Header20 = sheet[i][19],
+                            Header21 = sheet[i][20],
+                            Header22 = sheet[i][21],
+                            Header23 = sheet[i][22],
+                            Header24 = sheet[i][23],
+                            Header25 = sheet[i][24],
+                            Header26 = sheet[i][25],
+                            Header27 = sheet[i][26],
+                            Header28 = sheet[i][27],
+                            Header29 = sheet[i][28],
+                            Header30 = sheet[i][29],
+                            Header31 = sheet[i][30],
+                            Header32 = sheet[i][31],
+                            Header33 = sheet[i][32],
+                            Header34 = sheet[i][33],
+                            Header35 = sheet[i][34],
+                            Header36 = sheet[i][35],
+                            Header37 = sheet[i][36],
+                            Header38 = sheet[i][37],
+                            Header39 = sheet[i][38],
+                            Header40 = sheet[i][39],
+                            Header41 = sheet[i][40],
+                            Header42 = sheet[i][41],
+                            Header43 = sheet[i][42],
+                            Header44 = sheet[i][43],
+                            Header45 = sheet[i][44],
+                            Header46 = sheet[i][45],
+                            Header47 = sheet[i][46],
+                            Header48 = sheet[i][47],
+                            Header49 = sheet[i][48],
+                            Header50 = sheet[i][49],
+                            Header51 = sheet[i][50],
+                            Header52 = sheet[i][51],
+                            Header53 = sheet[i][52],
+                            Header54 = sheet[i][53],
+                            Header55 = sheet[i][54],
+                            Header56 = sheet[i][55],
+                            Header57 = sheet[i][56],
+                            Header58 = sheet[i][57],
+                            Header59 = sheet[i][58],
+                            Header60 = sheet[i][59],
+                            Header61 = sheet[i][60],
+                            Header62 = sheet[i][61],
+                            Header63 = sheet[i][62],
+                            Header64 = sheet[i][63],
+                            Header65 = sheet[i][64],
+                            Header66 = sheet[i][65],
+                            Header67 = sheet[i][66],
+                            Header68 = sheet[i][67],
+                            Header69 = sheet[i][68],
+                            Header70 = sheet[i][69],
+                            Header71 = sheet[i][70],
+                            Header72 = sheet[i][71],
+                            Header73 = sheet[i][72],
+                            Header74 = sheet[i][73],
+                            Header75 = sheet[i][74],
+                            Header76 = sheet[i][75],
+                            Header77 = sheet[i][76],
+                            Header78 = sheet[i][77],
+                            Header79 = sheet[i][78],
+                            Header80 = sheet[i][79],
+                            Header81 = sheet[i][80],
+                            Header82 = sheet[i][81],
+                            Header83 = sheet[i][82],
+                            Header84 = sheet[i][83],
+                            Header85 = sheet[i][84],
+                            Header86 = sheet[i][85],
+                            Header87 = sheet[i][86],
+                            Header88 = sheet[i][87],
+                            Header89 = sheet[i][88],
+                            Header90 = sheet[i][89],
+                            Header91 = sheet[i][90],
+                            Header92 = sheet[i][91],
+                            Header93 = sheet[i][92],
+                            Header94 = sheet[i][93],
+                            Header95 = sheet[i][94],
+                            Header96 = sheet[i][95],
+                            Header97 = sheet[i][96],
+                            Header98 = sheet[i][97],
+                            Header99 = sheet[i][98],
+                            Header100= sheet[i][99]
+                        }
+                    );
+                 
+                }
+            
+            }
+
+            DateTime endParseTime = DateTime.Now;
+
+            double Parse_elapsedTime = (endParseTime - startTime).TotalMilliseconds;
+
+
+            Console.Write(JsonConvert.SerializeObject(rowObj, Formatting.Indented));
+
+            DateTime endTime = DateTime.Now;
+
+            double elapsedTime = (endTime - startTime).TotalMilliseconds;
+
+            Console.Write("Parse ElapsedTime:" + Parse_elapsedTime + " ms");
+
+            Console.Write("Total ElapsedTime:" + elapsedTime + " ms");
+
+            Console.Read();
+
+        }
+    }
+}
